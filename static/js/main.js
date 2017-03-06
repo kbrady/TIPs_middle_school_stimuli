975a37dcd5936f52711c2abfd57151803e94cfa0
Brady <bradyka@vanderbilt.edu>
2017-03-06 14:26:20 -0600

    Fixing javascript for windows: text for each message must be less than 280 characters

--- static/js/main.js
+++ static/js/main.js
@@ -17,20 +17,35 @@ function read_out() {
 	}
 	var paragraphs = document.getElementById("content").children;
 	for (var i=0, max=paragraphs.length; i < max; i++) {
-  	say(paragraphs[i]);
+  		say(paragraphs[i]);
 	}
 }
 
 function say(my_element) {
-	var msg = new SpeechSynthesisUtterance(my_element.textContent);
-	msg.lang = "en-us";
-	msg.onstart = function (event) {
-		my_element.className += " highlighted";
-	};
-	var end_fun = function (event) {
-		my_element.className = my_element.className.substring(0, my_element.className.length - " highlighted".length);
-	};
-	msg.onend = end_fun;
-	msg.onerror = end_fun;
-	window.speechSynthesis.speak(msg);
+	var sentences = my_element.textContent.match( /[^\.!\?]+[\.!\?]*/g );
+	var text_snippets = [];
+	var current_string = "";
+	for (var i=0; i<sentences.length; i++) {
+		current_string += sentences[i];
+		if (i+1 == sentences.length || (current_string+sentences[i+1]).length > 250 ) {
+			text_snippets.push(current_string);
+			current_string = "";
+		}
+	}
+	for (var i=0; i<text_snippets.length; i++) {
+		var msg = new SpeechSynthesisUtterance();
+		msg.text = text_snippets[i];
+		msg.lang = "en-us";
+		if (i == 0) {
+			msg.onstart = function (event) {
+				my_element.className += " highlighted";
+			};
+		}
+		if (i+1 == text_snippets.length) {
+			msg.onend = function (event) {
+				my_element.className = my_element.className.substring(0, my_element.className.length - " highlighted".length);
+			};
+		}
+		window.speechSynthesis.speak(msg);
+	}
 }
